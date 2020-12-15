6656f2cf987565dddf413e4ff4edfd458bb68699
zc101211 <3384928772@qq.com>
2020-12-15 17:46:16 +0800

    首次提交

---
+++ plugs/tree_list/tree_data.js
@@ -0,0 +1,80 @@
+var dataJson = [{
+    "id": 5,
+    "department_name": "基础知识及语言表达",
+    "education_id": 0,
+    "parent_id": 0,
+    "created_at": "2018-05-17 08:59:48",
+    "updated_at": "2018-05-17 08:59:48",
+    "child": [{
+        "id": 18,
+        "department_name": "字音",
+        "education_id": 0,
+        "parent_id": 5,
+        "created_at": "2018-05-17 09:14:23",
+        "updated_at": "2018-05-17 09:14:23",
+        "child": [{
+            "id": 20,
+            "department_name": "多音字",
+            "education_id": 0,
+            "parent_id": 18,
+            "created_at": "2018-05-17 09:16:05",
+            "updated_at": "2018-05-17 09:16:05"
+        },{
+            "id": 21,
+            "department_name": "近音字",
+            "education_id": 0,
+            "parent_id": 19,
+            "created_at": "2018-05-17 09:16:05",
+            "updated_at": "2018-05-17 09:16:05"
+        }]
+    }, {
+        "id": 19,
+        "department_name": "字形",
+        "education_id": 0,
+        "parent_id": 5,
+        "created_at": "2018-05-17 09:14:31",
+        "updated_at": "2018-05-17 09:14:31"
+    }, {
+        "id": 19,
+        "department_name": "语法",
+        "education_id": 0,
+        "parent_id": 5,
+        "created_at": "2018-05-17 09:14:31",
+        "updated_at": "2018-05-17 09:14:31"
+    }]
+}, {
+    "id": 6,
+    "department_name": "文学常识及鉴赏",
+    "education_id": 0,
+    "parent_id": 0,
+    "created_at": "2018-05-17 08:59:58",
+    "updated_at": "2018-05-17 08:59:58"
+}, {
+    "id": 7,
+    "department_name": "文言文阅读",
+    "education_id": 0,
+    "parent_id": 0,
+    "created_at": "2018-05-17 09:00:44",
+    "updated_at": "2018-05-17 09:00:44"
+}, {
+    "id": 8,
+    "department_name": "现代文阅读",
+    "education_id": 0,
+    "parent_id": 0,
+    "created_at": "2018-05-17 09:00:53",
+    "updated_at": "2018-05-17 09:00:53"
+}, {
+    "id": 9,
+    "department_name": "默写，名著导读",
+    "education_id": 0,
+    "parent_id": 0,
+    "created_at": "2018-05-17 09:01:03",
+    "updated_at": "2018-05-17 09:01:03"
+}, {
+    "id": 10,
+    "department_name": "作文及写作",
+    "education_id": 0,
+    "parent_id": 0,
+    "created_at": "2018-05-17 09:01:12",
+    "updated_at": "2018-05-17 09:01:12"
+}]
